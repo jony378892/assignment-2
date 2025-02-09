f52f13268314c43e2fbd5053eed5e00d4f0742e9
Jony <jony378892@gmail.com>
2025-02-09 13:22:39 +0600

    Fixed some UI

--- src/components/Intruduction.jsx
+++ src/components/Intruduction.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 
 function Introduction({ handleStart, setName }) {
   const nameRef = useRef(null);
+  const [name, setNameInput] = useState("");
 
   function handleName(e) {
     e.preventDefault();
@@ -12,20 +13,23 @@ function Introduction({ handleStart, setName }) {
   return (
     <>
       <p className="text-lg mt-10 text-center">
-        {" "}
         ENTER YOUR NAME TO GET STARTED
       </p>
       <input
         type="text"
-        className="px-3 py-1.5  border border-blue-500 rounded-md bg-white"
-        placeholder="Your name"
-        required
+        className="px-3 py-1.5 border border-gray-400 rounded-md"
+        placeholder="John Doe"
         ref={nameRef}
+        value={name}
+        onChange={(e) => setNameInput(e.target.value)}
       />
       <button
         type="submit"
-        className="text-white bg-blue-500 px-4 py-1.5 mb-10 rounded-md cursor-pointer"
-        onClick={handleName}>
+        className={`text-white px-4 py-1.5 mb-10 rounded-md cursor-pointer ${
+          !name.trim() ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500"
+        }`}
+        onClick={handleName}
+        disabled={!name.trim()}>
         Submit
       </button>
     </>
